844eb8d6261c01c92567f6c793dbf5c378f03a3a
Vasiliy Konovalov <beenbrand@mail.ru>
2019-03-13 20:03:14 +0500

    правки кода

--- src/OverlookedSlider.js
+++ src/OverlookedSlider.js
@@ -9,8 +9,8 @@ import JSONproducts from './data/products.json';
 import PropTypes from 'prop-types';
 
 //!!! закинули в sessionStorage просмотренные id (это временный код для разработки)
-let massiv = [20, 21, 22,23,24,25,26,27,28,29,30,31,32,33,34,35];
-sessionStorage.overlooked = JSON.stringify(massiv);
+let overlookedIds = [20, 21, 22,23,24,25,26,27,28,29,30,31,32,33,34,35];
+sessionStorage.overlooked = JSON.stringify(overlookedIds);
 
 class OverlookedSlider extends Component {
   constructor(props) {
